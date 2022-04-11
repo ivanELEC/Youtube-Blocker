a211f74249a602ef6266fd906bcbe61a19e8f611
Ivan Yohuno <ivan_yohuno@hotmail.co.uk>
2022-04-11 20:54:03 +0100

    Refactored code into functions

--- js/block_accounts.js
+++ js/block_accounts.js
@@ -3,20 +3,27 @@ window.addEventListener('DOMContentLoaded', function(){
 	var addButtonElem = document.getElementById('block_add_button');
 	//handle account submission
 	addButtonElem.addEventListener('click', function(){
-		//get account name from input 
-		var inputVal = document.getElementById('block_account_name').value;
-		//clone dropdown list item template
-		var listItemTemplate = document.getElementById('dropdown-li');
-		var clone = listItemTemplate.content.cloneNode(true);
-
-		//set account name in list item
-		var cloneLinkNode = clone.querySelectorAll('a');
-		cloneLinkNode = cloneLinkNode[0];
-		cloneLinkNode.innerText = inputVal;
-
-		//insert clone into dropdown list
-		var dropdownElem = document.getElementById('accounts-dropdown');
-		dropdownElem.appendChild(clone);
+		submitAccount();
 	});
 })
 
+function updateDropdownList(accountName){
+	//clone dropdown list item template
+	var listItemTemplate = document.getElementById('dropdown-li');
+	var clone = listItemTemplate.content.cloneNode(true);
+
+	//set account name in list item
+	var cloneLinkNode = clone.querySelectorAll('a');
+	cloneLinkNode = cloneLinkNode[0];
+	cloneLinkNode.innerText = accountName;
+
+	//insert clone into dropdown list
+	var dropdownElem = document.getElementById('accounts-dropdown');
+	dropdownElem.appendChild(clone);
+}
+
+function submitAccount(){
+	//get account name from input 
+	var inputVal = document.getElementById('block_account_name').value;
+	updateDropdownList(inputVal);
+}
