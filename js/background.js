6bc86846a1806d1c6c5e793ee3ddf15f46dfa5dc
Ivan Yohuno <ivan_yohuno@hotmail.co.uk>
2022-04-11 20:46:42 +0100

    Basic dropdown UI interactivity
    Added ability to add items to dropdown from form
    Changed CSS to make sizing of frame suitable
    Removed redundant permissions

--- js/background.js
+++ js/background.js
@@ -1,7 +1,10 @@
 //array of YouTube accounts to block
-let block_list = [];
+const block_list = [];
 
 //initialise blocklist from default
 chrome.runtime.onInstalled.addListener(() => {
-	chrome.storage.sync.set({block_list})
+	chrome.storage.sync.set({'block_list':block_list}, function(){
+		console.log('block_list is:');
+		console.log(block_list);
+	})
 });
--- js/block_accounts.js
+++ js/block_accounts.js
@@ -1,29 +1,22 @@
 //event listeners
-window.addEventListener('DOMContentLoaded', (event) =>{
+window.addEventListener('DOMContentLoaded', function(){
 	var addButtonElem = document.getElementById('block_add_button');
-	addButtonElem.onclick = submitAccount;
+	//handle account submission
+	addButtonElem.addEventListener('click', function(){
+		//get account name from input 
+		var inputVal = document.getElementById('block_account_name').value;
+		//clone dropdown list item template
+		var listItemTemplate = document.getElementById('dropdown-li');
+		var clone = listItemTemplate.content.cloneNode(true);
+
+		//set account name in list item
+		var cloneLinkNode = clone.querySelectorAll('a');
+		cloneLinkNode = cloneLinkNode[0];
+		cloneLinkNode.innerText = inputVal;
+
+		//insert clone into dropdown list
+		var dropdownElem = document.getElementById('accounts-dropdown');
+		dropdownElem.appendChild(clone);
+	});
 })
-
-//functions
-function addToBlockDropdown(accountName){
-	//clone dropdown list item template
-	var listItemTemplate = document.getElementById('dropdown-li');
-	var clone = listItemTemplate.content.cloneNode(true);
-
-	//set account name in list item
-	var cloneLinkNode = clone.querySelectorAll('a');
-	cloneLinkNode = cloneLinkNode[0];
-	cloneLinkNode.innerText = accountName;
-
-	//insert clone into dropdown list
-	var dropdownElem = document.getElementById('accounts-dropdown');
-	dropdownElem.appendChild(clone);
-	
-}
-
-function submitAccount(){
-	console.log('test');
-	var inputVal = document.getElementById('block_account_name').value;
-	addToBlockDropdown(inputVal);
-}
 
