24c469a64f6248d1e2b6416ab56823d4dcd440c9
Ivan Yohuno <ivan_yohuno@hotmail.co.uk>
2022-04-21 22:30:03 +0100

    Use Mutation Observer to filter on change
    Mutation Observer checks for changes in the child list of contents and runs anytime there is a change
    
    Can add more conditions to reduce unnecessary processing

--- js/content_scripts/block_accounts.js
+++ js/content_scripts/block_accounts.js
@@ -1,12 +1,29 @@
 //handle list of accounts to block
 chrome.storage.sync.get("block_list", ({block_list}) => {
-	filterVideosInHome(block_list)
+	const contentsElement = document.getElementById("contents");
+	
+	//setup mutation observer to check for changes in child elements of contents
+	const observerConfig = {attributes: false, childList: true, subtree: true};
+	
+	//function for applying filter anytime child node added
+	const callback = function(mutationsList, observer){
+		for(const mutation of mutationsList){
+			if(mutation.type === 'childList'){
+				filterVideosInHome(block_list);
+			}
+		}
+	}
+	
+	//create observer and start it
+	const observer = new MutationObserver(callback);
+	observer.observe(contentsElement, observerConfig);
 });
 
 //filters videos made by any account name in account_list in the YouTube homepage
 function filterVideosInHome(account_list){
 	//select channel name divs in block list
 	let channels = getChannelNameDivsHome(account_list);
+	
 	/*for each element found remove closest div with id content
 		note: this assumes that closes div (child or parent) is the parent one that 
 		contains the video and thumbnail
@@ -17,12 +34,14 @@ function filterVideosInHome(account_list){
 	}
 }
 
+
 //function to select all title elements that have list of channels
 function getChannelNameDivsHome(account_list){
 	//to store elements with a channel name
 	var elementArray = []; 
 	let contentElement = document.getElementById("contents");
 	let channelElements = Array.from(contentElement.querySelectorAll('.style-scope ytd-channel-name'));
+	
 	for(let i=0; i < channelElements.length; i++){
 		//add to elementArray if element's inner text is included in account_list array
 		account_list.find(account =>{
@@ -31,6 +50,7 @@ function getChannelNameDivsHome(account_list){
 			}
 		});
 	};
+	
 	return elementArray;
 }
 
